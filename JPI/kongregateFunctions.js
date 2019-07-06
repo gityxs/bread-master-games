316ac8bd9e94af4e607062c2a786bde04cd94c23
Luke <thereallmwels@gmail.com>
2019-07-06 12:37:35 -0400

    first commit

---
+++ JPI/kongregateFunctions.js
@@ -0,0 +1,20 @@
+﻿window.kongregateFunctions = {
+
+    getUsername:function() {
+        // You can now access the Kongregate API with:
+        // kongregate.services.getUsername(), etc
+        // Proceed with loading your game...
+        
+        if (kongregate.services.isGuest()) {
+            return "You are a guest.";
+        } else {
+            return kongregate.services.getUsername();
+        }
+    },
+    updateTotalLevelScore: function (totalLevel) {
+        kongregate.stats.submit("Total Level", totalLevel);
+    },
+    createSortableList: function (listElement) {
+        Sortable.create(listElement, {});
+    }
+};
