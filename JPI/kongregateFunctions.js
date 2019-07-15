f25282050d5654689d1b68ab9b3ca2846f6a6299
Luke <thereallmwels@gmail.com>
2019-07-14 22:21:17 -0400

    api try 3

--- JPI/kongregateFunctions.js
+++ JPI/kongregateFunctions.js
@@ -11,6 +11,17 @@
             return window.kongregate.services.getUsername();
         }
     },
+    getUserID: function () {
+        // You can now access the Kongregate API with:
+        // kongregate.services.getUsername(), etc
+        // Proceed with loading your game...
+
+        if (window.kongregate.services.isGuest()) {
+            return "You are a guest.";
+        } else {
+            return window.kongregate.services.getUserId();
+        }
+    },
     updateTotalLevelScore: function (totalLevel) {
         window.kongregate.stats.submit("Total Level", totalLevel);
     },
@@ -26,12 +37,12 @@
             return "";
         }
         else {
-            return window.kongregate.services.getGameAuthToken();
+            return windowkongregate.services.getGameAuthToken();
         }
     },
     LoginWithKongregateID:function(){
         PlayFab.settings.titleId = "1A3BB";
-        var username = this.getUsername();
+        var username = this.getUserID();
         var token = this.getToken();
         var loginRequest = {
         // Currently, you need to look up the correct format for this object in the API-docs:
